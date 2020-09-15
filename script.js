59ec0877b3a1a86d66f879b5e4a36a1a7bbfb412
David Guthmann <drguthmann@cox.net>
2020-09-14 22:07:22 -0500

    Test Question layout, working on functions, bugs...

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ var timeClock = document.querySelector(".timer");
 var userScore = document.querySelector(".score");
 // set variable for Progress of Quiz
 var progress = document.querySelector(".quizLength");
+// set variable for Quiz End
+var finished = document.querySelector(".finished");
 // Write in Questions (array of objects(Question: , ChoiceA:, Choice2:, Choice3:, Choice4:, CorrectAnswer : 1))
 var questions = [
     {
@@ -27,22 +29,29 @@ var questions = [
         correct: "A"
     },
     {
-        question: "",
-        choiceA: "",
-        choiceB: "",
-        choiceC: "",
-        choiceD: "",
-        correct: ""
+        question: "Example Question",
+        choiceA: "A is right",
+        choiceB: "B is wrong",
+        choiceC: "C is wrong",
+        choiceD: "D is wrong",
+        correct: "A"
+    },
+    {
+        question: "Example Question",
+        choiceA: "A is right",
+        choiceB: "B is wrong",
+        choiceC: "C is wrong",
+        choiceD: "D is wrong",
+        correct: "A"
     }
 ]
 
 // Create Function for generating the questions on the screen
-
 var questionTotalIndex = questions.length;
 var questionCurrentIndex = 0;
 
-function askQuestion(){
-    var q = question[questionCurrentIndex];
+function askQuestion() {
+    var q = questions[questionCurrentIndex];
     quiz.textContent = q.question;
     choiceA.textContent = q.choiceA;
     choiceB.textContent = q.choiceB;
@@ -50,30 +59,38 @@ function askQuestion(){
     choiceD.textContent = q.choiceD;
 }
 
+// Create Function for Listing Question X of Y
+function quizProgress() {
+    document.querySelector(".quizLength").textContent = `Question: ${questionCurrentIndex+1} of ${questionTotalIndex}`;
+}
+
 // Create a Function to check if answer is right or wrong
-function answerChecker(answer){
-    if(questions[questionCurrentIndex].correct === answer){
+function answerChecker(answer) {
+    if (questions[questionCurrentIndex].correct === answer) {
         userScore++;
         correctAnswer();
     } else {
-        timeTotal += 10;
+        timeLeft += 10;
         wrongAnswer();
     }
-    if(questionCurrentIndex < questionTotalIndex){
+    if (questionCurrentIndex < questionTotalIndex) {
         questionCurrentIndex++;
-        askQuestion
+        quizProgress();
+        askQuestion();
+    } else {
+        endQuiz();
     }
 }
 
 // Create Function to display correct answer for user
-function correctAnswer(){
+function correctAnswer() {
     // Display Correct for user
     document.querySelector(".validity").textContent = "Correct!"
 
 }
 
 // Create Function to display incorrect answer for user
-function wrongAnswer(){
+function wrongAnswer() {
     // Display User incorrect
     document.querySelector(".validity").textContent = "Incorrect!"
 }
@@ -82,22 +99,39 @@ var timeTotal = 90;
 var timeLeft = 0;
 
 // Create running display counter, when time used is the same as time left end quiz
-function timerDisplay(){
-    if(timeLeft <= timeTotal){
-        timeClock.textContent = (timeTotal-timeLeft)+" Seconds Left";
+function timerDisplay() {
+    if (timeLeft <= timeTotal) {
+        timeClock.textContent = (timeTotal - timeLeft) + " Seconds Left";
         timeLeft++;
     } else {
+        clearInterval(timerValue);
         timeLeft = 0;
         endQuiz();
     }
 }
 
-setInterval(timerDisplay, 1000);
+
 
 // Create a Function to End the Quiz
-function endQuiz(){
+function endQuiz() {
     //Way 1: User Answered All Questions
-
+    clearInterval(timerValue);
+    timeLeft = 0;
     //Way 2: User is out of time
-
+    quiz.style.display = "none";
+    finished.style.display = "block";
 }
+
+var timerValue;
+// Function to Start the quiz
+function startQuiz() {
+    start.style.display = "none";
+    quiz.style.display = "block";
+    timerDisplay();
+    timerValue = setInterval(timerDisplay, 1000);
+    quizProgress();
+    askQuestion();
+}
+// Clickable Event to start quiz
+var start = document.querySelector(".start");
+start.addEventListener("click", startQuiz);
